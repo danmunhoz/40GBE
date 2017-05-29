d4d49ef15e926ac2237e142ab92b8bcb48c8e616
GuilhermeKorol <korol.guilherme@gmail.com>
2017-05-29 14:59:23 -0300

    Scoreboard print ok

--- Hardware/sim/scoreboard.py
+++ Hardware/sim/scoreboard.py
@@ -1,6 +1,7 @@
 f0  = open("dump_mii_tx.txt", "r")
 f1  = open("dump_mii_rx.txt", "r")
 i = 0
+error = 0
 
 for line0, line1 in zip(f0, f1):
         i = i + 1
@@ -8,3 +9,9 @@ for line0, line1 in zip(f0, f1):
             print "Error: MII_TX and MII_RX are different at line %d!" % i
             print "MII_TX line: ",line0,"MII_RX line: ",line1
             print " "
+            error = error + 1;
+
+if not(error > 0):
+    print "[SCOREBOARD] dump_mii_tx == dump_mii_rx"
+else:
+    print "[SCOREBOARD] %d errors founded!" %error
