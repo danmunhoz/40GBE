9298f7fd311a636e7045aa455c678f1c78e47d40
Rafael Sperb <rafasperb@gmail.com>
2017-10-17 11:29:28 -0200

    adicionando scripts para comparação entre entrada e saida.

---
+++ Hardware/sim/clean_dump_tx.py
@@ -0,0 +1,59 @@
+"""
+Created on Mon Oct 16 16:12:43 2017
+
+@author: rafasperb
+"""
+import os
+
+control = []
+data = []
+escreve = 0
+sop_addr = 0
+achou_eop = 0
+new_file = open('cleaned_dump_tx2.txt', 'w')
+new_file2 = open('cleaned_dump_tx.txt', 'w')
+
+with open('dump_mii_tx.txt') as f:
+    for line in f:
+        control = line[0:8]
+        data = line[9:]
+
+        if escreve == 0:
+
+            if control == "11111111":
+                escreve = 0
+            else:
+
+                sop_addr = int(control.index('1'))
+                if data[(sop_addr*8):(sop_addr*8+8)] == "11111011": #significa que de fato eh sop
+                    if sop_addr == 7:
+                        escreve = 1
+                    elif sop_addr == 3:
+                        escreve = 2
+
+        elif escreve == 2:
+            new_file.write(data[:32]+"\n")
+            escreve = 1
+
+        elif escreve == 1:
+            #testar se tem EOP
+            if control == "00000000":
+                new_file.write(data)
+            else:
+                for i in range(8):
+                    if control[i] == "1" and data[(i*8):(i*8+8)] == "11111101":
+                        new_file.write(data[(i*8+8):])
+                        escreve = 0
+                        break
+
+new_file.close()
+
+with open('cleaned_dump_tx2.txt') as f:
+    for line in f:
+        if line[32:] != "\n":
+            new_file2.write(line[32:])
+        if line[:32] != "\n":
+            new_file2.write(line[:32]+"\n")
+new_file2.close()
+
+os.remove('cleaned_dump_tx2.txt')
