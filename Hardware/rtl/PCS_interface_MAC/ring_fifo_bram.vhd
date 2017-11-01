a054baac1708260af6aa6b19602c79ef5c6fc49d
GuilhermeKorol <korol.guilherme@gmail.com>
2017-11-01 14:42:18 -0200

    Mudanca de macro das fifos do mac interface

--- Hardware/rtl/PCS_interface_MAC/ring_fifo_bram.vhd
+++ Hardware/rtl/PCS_interface_MAC/ring_fifo_bram.vhd
@@ -75,6 +75,15 @@ architecture behav of ring_fifo_bram is
   signal mem_high_in_0_reg : std_logic_vector(63 downto 0);
   signal mem_high_in_1_reg : std_logic_vector(71 downto 0);
 
+  signal rdcount_l_0 : std_logic_vector(8 downto 0);
+  signal wrcount_l_0 : std_logic_vector(8 downto 0);
+  signal rdcount_h_0 : std_logic_vector(8 downto 0);
+  signal wrcount_h_0 : std_logic_vector(8 downto 0);
+  signal rdcount_l_1 : std_logic_vector(8 downto 0);
+  signal wrcount_l_1 : std_logic_vector(8 downto 0);
+  signal rdcount_h_1 : std_logic_vector(8 downto 0);
+  signal wrcount_h_1 : std_logic_vector(8 downto 0);
+
 begin
   rst <= not rst_n;
 
@@ -122,105 +131,242 @@ begin
 
   -- is_sop_out <= mem_high_out_1(0) when rr = '0' else '0';
 
-  BRAM_inst_l_0 : BRAM_SDP_MACRO
+  -- BRAM_inst_l_0 : BRAM_SDP_MACRO
+  --   generic map (
+  --     BRAM_SIZE => "36Kb",            -- Target BRAM, "18Kb" or "36Kb"
+  --     DEVICE => "7SERIES",            -- Target device: "VIRTEX5", "VIRTEX6", "7SERIES", "SPARTAN6"
+  --     WRITE_WIDTH => 64,              -- Valid values are 1-72 (37-72 only valid when BRAM_SIZE="36Kb")
+  --     READ_WIDTH => 64,               -- Valid values are 1-72 (37-72 only valid when BRAM_SIZE="36Kb")
+  --     DO_REG => 0,                    -- Optional output register (0 or 1)
+  --     INIT_FILE => "NONE",
+  --     SIM_COLLISION_CHECK => "ALL",   -- Collision check enable "ALL", "WARNING_ONLY",
+  --     SRVAL => X"000000000000000000", -- Set/Reset value for port output
+  --     WRITE_MODE => "WRITE_FIRST")    -- Specify "WRITE_FIRST for asynchrononous clocks on ports
+  --   port map(
+  --     DO => mem_low_out_0,            -- Output read data port, width defined by READ_WIDTH parameter
+  --     DI => mem_low_in_0_reg,             -- Input write data port, width defined by WRITE_WIDTH parameter
+  --     RDADDR => r_ptr_l,              -- Input read address, width defined by read port depth
+  --     RDCLK => clk_r,                 -- 1-bit input read clock
+  --     RDEN => ren_int_l,              -- 1-bit input read port enable
+  --     REGCE => '0',                   -- 1-bit input read output register enable
+  --     RST => rst,                     -- 1-bit input reset
+  --     WE => ARRAY_8_ONES,             -- Input write enable, width defined by write port depth
+  --     WRADDR => w_ptr,                -- Input write address, width defined by write port depth
+  --     WRCLK => clk_w,                 -- 1-bit input write clock
+  --     WREN => wen_bram                     -- 1-bit input write port enable
+  --     );
+
+  FIFO_inst_l_0 : FIFO_DUALCLOCK_MACRO
     generic map (
-      BRAM_SIZE => "36Kb",            -- Target BRAM, "18Kb" or "36Kb"
-      DEVICE => "7SERIES",            -- Target device: "VIRTEX5", "VIRTEX6", "7SERIES", "SPARTAN6"
-      WRITE_WIDTH => 64,              -- Valid values are 1-72 (37-72 only valid when BRAM_SIZE="36Kb")
-      READ_WIDTH => 64,               -- Valid values are 1-72 (37-72 only valid when BRAM_SIZE="36Kb")
-      DO_REG => 0,                    -- Optional output register (0 or 1)
-      INIT_FILE => "NONE",
-      SIM_COLLISION_CHECK => "ALL",   -- Collision check enable "ALL", "WARNING_ONLY",
-      SRVAL => X"000000000000000000", -- Set/Reset value for port output
-      WRITE_MODE => "WRITE_FIRST")    -- Specify "WRITE_FIRST for asynchrononous clocks on ports
-    port map(
-      DO => mem_low_out_0,            -- Output read data port, width defined by READ_WIDTH parameter
-      DI => mem_low_in_0_reg,             -- Input write data port, width defined by WRITE_WIDTH parameter
-      RDADDR => r_ptr_l,              -- Input read address, width defined by read port depth
-      RDCLK => clk_r,                 -- 1-bit input read clock
-      RDEN => ren_int_l,              -- 1-bit input read port enable
-      REGCE => '0',                   -- 1-bit input read output register enable
-      RST => rst,                     -- 1-bit input reset
-      WE => ARRAY_8_ONES,             -- Input write enable, width defined by write port depth
-      WRADDR => w_ptr,                -- Input write address, width defined by write port depth
-      WRCLK => clk_w,                 -- 1-bit input write clock
-      WREN => wen_bram                     -- 1-bit input write port enable
+      DEVICE => "7SERIES", -- Target Device: "VIRTEX5", "VIRTEX6", "7SERIES"
+      ALMOST_FULL_OFFSET => X"0080", -- Sets almost full threshold
+      ALMOST_EMPTY_OFFSET => X"0080", -- Sets the almost empty threshold
+      DATA_WIDTH => 64, -- Valid values are 1-72 (37-72 only valid when FIFO_SIZE="36Kb")
+      FIFO_SIZE => "36Kb", -- Target BRAM, "18Kb" or "36Kb"
+      FIRST_WORD_FALL_THROUGH => FALSE) -- Sets the FIFO FWFT to TRUE or FALSE
+    port map (
+      ALMOSTEMPTY => open, -- 1-bit output almost empty
+      ALMOSTFULL => open, -- 1-bit output almost full
+      DO => mem_low_out_0, -- Output data, width defined by DATA_WIDTH parameter
+      EMPTY => open, -- 1-bit output empty
+      FULL => open, -- 1-bit output full
+      RDCOUNT => rdcount_l_0, -- Output read count, width determined by FIFO depth
+      RDERR => open, -- 1-bit output read error
+      WRCOUNT => wrcount_l_0, -- Output write count, width determined by FIFO depth
+      WRERR => open, -- 1-bit output write error
+      DI => mem_low_in_0_reg, -- Input data, width defined by DATA_WIDTH parameter
+      RDCLK => clk_r, -- 1-bit input read clock
+      RDEN => ren_int_l, -- 1-bit input read enable
+      RST => rst, -- 1-bit input reset
+      WRCLK => clk_w, -- 1-bit input write clock
+      WREN => wen_bram -- 1-bit input write enable
+    );
+
+
+
+  -- BRAM_TDP_MACRO_inst : BRAM_TDP_MACRO
+  --   generic map (
+  --     BRAM_SIZE => "36Kb",            -- Target BRAM, "18Kb" or "36Kb"
+  --     DEVICE => "7SERIES",            -- Target Device: "VIRTEX5", "VIRTEX6", "7SERIES", "SPARTAN6"
+  --     DOA_REG => 0, DOB_REG => 0,     -- Optional port A/B output register (0 or 1)
+  --     INIT_A => X"000000000", INIT_B => X"000000000",         -- Initial values on A/B output port
+  --     INIT_FILE => "NONE",
+  --     READ_WIDTH_A => 32,              -- Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
+  --     READ_WIDTH_B => 32,              -- Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
+  --     SIM_COLLISION_CHECK => "ALL",   -- Collision check enable "ALL", "WARNING_ONLY", "GENERATE_X_ONLY" or "NONE"
+  --     SRVAL_A => X"000000000", SRVAL_B => X"000000000",        -- Set/Reset value for A/B port output
+  --     WRITE_MODE_A => "WRITE_FIRST", WRITE_MODE_B => "WRITE_FIRST", -- "WRITE_FIRST", "READ_FIRST" or "NO_CHANGE"
+  --     WRITE_WIDTH_A => 32,             -- Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
+  --     WRITE_WIDTH_B => 32)             -- Valid values are 1-36 (19-36 only valid when BRAM_SIZE="36Kb")
+  --   port map (
+  --     DOA => DOA,                     -- Output port-A data, width defined by READ_WIDTH_A parameter
+  --     DOB => DOB,                     -- Output port-B data, width defined by READ_WIDTH_B parameter
+  --     ADDRA => ADDRA,                 -- Input port-A address, width defined by Port A depth
+  --     ADDRB => ADDRB,                 -- Input port-B address, width defined by Port B depth
+  --     CLKA => CLKA,                   -- 1-bit input port-A clock
+  --     CLKB => CLKB,                   -- 1-bit input port-B clock
+  --     DIA => DIA,                     -- Input port-A data, width defined by WRITE_WIDTH_A parameter
+  --     DIB => DIB,                     -- Input port-B data, width defined by WRITE_WIDTH_B parameter
+  --     ENA => ENA,                     -- 1-bit input port-A enable
+  --     ENB => ENB,                     -- 1-bit input port-B enable
+  --     REGCEA => REGCEA,               -- 1-bit input port-A output register enable
+  --     REGCEB => REGCEB,               -- 1-bit input port-B output register enable
+  --     RSTA => RSTA,                   -- 1-bit input port-A reset
+  --     RSTB => RSTB,                   -- 1-bit input port-B reset
+  --     WEA => WEA,                     -- Input port-A write enable, width defined by Port A depth
+  --     WEB => WEB                      -- Input port-B write enable, width defined by Port B depth
+    -- );
+
+
+  -- BRAM_inst_l_1 : BRAM_SDP_MACRO
+  --   generic map (
+  --     BRAM_SIZE => "36Kb",
+  --     DEVICE => "7SERIES",
+  --     WRITE_WIDTH => 72,                -- 64 (data) + 5 (eop location) + 1(sop)
+  --     READ_WIDTH => 72,
+  --     DO_REG => 0,
+  --     INIT_FILE => "NONE",
+  --     SIM_COLLISION_CHECK => "ALL",
+  --     SRVAL => X"000000000000000000",
+  --     WRITE_MODE => "WRITE_FIRST")
+  --   port map(
+  --     DO => mem_low_out_1,
+  --     DI => mem_low_in_1_reg,
+  --     RDADDR => r_ptr_l,
+  --     RDCLK => clk_r,
+  --     RDEN => ren_int_l,
+  --     REGCE => '0',
+  --     RST => rst,
+  --     WE => ARRAY_8_ONES,
+  --     WRADDR => w_ptr,
+  --     WRCLK => clk_w,
+  --     WREN => wen_bram
+  --   );
+    FIFO_inst_l_1 : FIFO_DUALCLOCK_MACRO
+      generic map (
+        DEVICE => "7SERIES", -- Target Device: "VIRTEX5", "VIRTEX6", "7SERIES"
+        ALMOST_FULL_OFFSET => X"0080", -- Sets almost full threshold
+        ALMOST_EMPTY_OFFSET => X"0080", -- Sets the almost empty threshold
+        DATA_WIDTH => 72, -- Valid values are 1-72 (37-72 only valid when FIFO_SIZE="36Kb")
+        FIFO_SIZE => "36Kb", -- Target BRAM, "18Kb" or "36Kb"
+        FIRST_WORD_FALL_THROUGH => FALSE) -- Sets the FIFO FWFT to TRUE or FALSE
+      port map (
+        ALMOSTEMPTY => open, -- 1-bit output almost empty
+        ALMOSTFULL => open, -- 1-bit output almost full
+        DO => mem_low_out_1, -- Output data, width defined by DATA_WIDTH parameter
+        EMPTY => open, -- 1-bit output empty
+        FULL => open, -- 1-bit output full
+        RDCOUNT => rdcount_l_1, -- Output read count, width determined by FIFO depth
+        RDERR => open, -- 1-bit output read error
+        WRCOUNT => wrcount_l_1, -- Output write count, width determined by FIFO depth
+        WRERR => open, -- 1-bit output write error
+        DI => mem_low_in_1_reg, -- Input data, width defined by DATA_WIDTH parameter
+        RDCLK => clk_r, -- 1-bit input read clock
+        RDEN => ren_int_l, -- 1-bit input read enable
+        RST => rst, -- 1-bit input reset
+        WRCLK => clk_w, -- 1-bit input write clock
+        WREN => wen_bram -- 1-bit input write enable
       );
 
-  BRAM_inst_l_1 : BRAM_SDP_MACRO
-    generic map (
-      BRAM_SIZE => "36Kb",
-      DEVICE => "7SERIES",
-      WRITE_WIDTH => 72,                -- 64 (data) + 5 (eop location) + 1(sop)
-      READ_WIDTH => 72,
-      DO_REG => 0,
-      INIT_FILE => "NONE",
-      SIM_COLLISION_CHECK => "ALL",
-      SRVAL => X"000000000000000000",
-      WRITE_MODE => "WRITE_FIRST")
-    port map(
-      DO => mem_low_out_1,
-      DI => mem_low_in_1_reg,
-      RDADDR => r_ptr_l,
-      RDCLK => clk_r,
-      RDEN => ren_int_l,
-      REGCE => '0',
-      RST => rst,
-      WE => ARRAY_8_ONES,
-      WRADDR => w_ptr,
-      WRCLK => clk_w,
-      WREN => wen_bram
-    );
+  -- BRAM_inst_h_0 : BRAM_SDP_MACRO
+  --   generic map (
+  --     BRAM_SIZE => "36Kb",
+  --     DEVICE => "7SERIES",
+  --     WRITE_WIDTH => 64,
+  --     READ_WIDTH => 64,
+  --     DO_REG => 0,
+  --     INIT_FILE => "NONE",
+  --     SIM_COLLISION_CHECK => "ALL",
+  --     SRVAL => X"000000000000000000",
+  --     WRITE_MODE => "WRITE_FIRST")
+  --   port map(
+  --     DO => mem_high_out_0,
+  --     DI => mem_high_in_0_reg,
+  --     RDADDR => r_ptr_h,
+  --     RDCLK => clk_r,
+  --     RDEN => ren_int_h,
+  --     REGCE => '0',
+  --     RST => rst,
+  --     WE => ARRAY_8_ONES,
+  --     WRADDR => w_ptr,
+  --     WRCLK => clk_w,
+  --     WREN => wen_bram
+  --   );
+    FIFO_inst_h_0 : FIFO_DUALCLOCK_MACRO
+      generic map (
+        DEVICE => "7SERIES", -- Target Device: "VIRTEX5", "VIRTEX6", "7SERIES"
+        ALMOST_FULL_OFFSET => X"0080", -- Sets almost full threshold
+        ALMOST_EMPTY_OFFSET => X"0080", -- Sets the almost empty threshold
+        DATA_WIDTH => 64, -- Valid values are 1-72 (37-72 only valid when FIFO_SIZE="36Kb")
+        FIFO_SIZE => "36Kb", -- Target BRAM, "18Kb" or "36Kb"
+        FIRST_WORD_FALL_THROUGH => FALSE) -- Sets the FIFO FWFT to TRUE or FALSE
+      port map (
+        ALMOSTEMPTY => open, -- 1-bit output almost empty
+        ALMOSTFULL => open, -- 1-bit output almost full
+        DO => mem_high_out_0, -- Output data, width defined by DATA_WIDTH parameter
+        EMPTY => open, -- 1-bit output empty
+        FULL => open, -- 1-bit output full
+        RDCOUNT => rdcount_h_0, -- Output read count, width determined by FIFO depth
+        RDERR => open, -- 1-bit output read error
+        WRCOUNT => wrcount_h_0, -- Output write count, width determined by FIFO depth
+        WRERR => open, -- 1-bit output write error
+        DI => mem_high_in_0_reg, -- Input data, width defined by DATA_WIDTH parameter
+        RDCLK => clk_r, -- 1-bit input read clock
+        RDEN => ren_int_h, -- 1-bit input read enable
+        RST => rst, -- 1-bit input reset
+        WRCLK => clk_w, -- 1-bit input write clock
+        WREN => wen_bram -- 1-bit input write enable
+      );
 
-  BRAM_inst_h_0 : BRAM_SDP_MACRO
-    generic map (
-      BRAM_SIZE => "36Kb",
-      DEVICE => "7SERIES",
-      WRITE_WIDTH => 64,
-      READ_WIDTH => 64,
-      DO_REG => 0,
-      INIT_FILE => "NONE",
-      SIM_COLLISION_CHECK => "ALL",
-      SRVAL => X"000000000000000000",
-      WRITE_MODE => "WRITE_FIRST")
-    port map(
-      DO => mem_high_out_0,
-      DI => mem_high_in_0_reg,
-      RDADDR => r_ptr_h,
-      RDCLK => clk_r,
-      RDEN => ren_int_h,
-      REGCE => '0',
-      RST => rst,
-      WE => ARRAY_8_ONES,
-      WRADDR => w_ptr,
-      WRCLK => clk_w,
-      WREN => wen_bram
-    );
-
-  BRAM_inst_h_1 : BRAM_SDP_MACRO
-    generic map (
-      BRAM_SIZE => "36Kb",
-      DEVICE => "7SERIES",
-      WRITE_WIDTH => 72,                         -- 64 (data) + 5 (eop location)
-      READ_WIDTH => 72,
-      DO_REG => 0,
-      INIT_FILE => "NONE",
-      SIM_COLLISION_CHECK => "ALL",
-      SRVAL => X"000000000000000000",
-      WRITE_MODE => "WRITE_FIRST")
-    port map(
-      DO => mem_high_out_1,
-      DI => mem_high_in_1_reg,
-      RDADDR => r_ptr_h,
-      RDCLK => clk_r,
-      RDEN => ren_int_h,
-      REGCE => '0',
-      RST => rst,
-      WE => ARRAY_8_ONES,
-      WRADDR => w_ptr,
-      WRCLK => clk_w,
-      WREN => wen_bram
-    );
+  -- BRAM_inst_h_1 : BRAM_SDP_MACRO
+  --   generic map (
+  --     BRAM_SIZE => "36Kb",
+  --     DEVICE => "7SERIES",
+  --     WRITE_WIDTH => 72,                         -- 64 (data) + 5 (eop location)
+  --     READ_WIDTH => 72,
+  --     DO_REG => 0,
+  --     INIT_FILE => "NONE",
+  --     SIM_COLLISION_CHECK => "ALL",
+  --     SRVAL => X"000000000000000000",
+  --     WRITE_MODE => "WRITE_FIRST")
+  --   port map(
+  --     DO => mem_high_out_1,
+  --     DI => mem_high_in_1_reg,
+  --     RDADDR => r_ptr_h,
+  --     RDCLK => clk_r,
+  --     RDEN => ren_int_h,
+  --     REGCE => '0',
+  --     RST => rst,
+  --     WE => ARRAY_8_ONES,
+  --     WRADDR => w_ptr,
+  --     WRCLK => clk_w,
+  --     WREN => wen_bram
+  --   );
+    FIFO_inst_h_1 : FIFO_DUALCLOCK_MACRO
+      generic map (
+        DEVICE => "7SERIES", -- Target Device: "VIRTEX5", "VIRTEX6", "7SERIES"
+        ALMOST_FULL_OFFSET => X"0080", -- Sets almost full threshold
+        ALMOST_EMPTY_OFFSET => X"0080", -- Sets the almost empty threshold
+        DATA_WIDTH => 72, -- Valid values are 1-72 (37-72 only valid when FIFO_SIZE="36Kb")
+        FIFO_SIZE => "36Kb", -- Target BRAM, "18Kb" or "36Kb"
+        FIRST_WORD_FALL_THROUGH => FALSE) -- Sets the FIFO FWFT to TRUE or FALSE
+      port map (
+        ALMOSTEMPTY => open, -- 1-bit output almost empty
+        ALMOSTFULL => open, -- 1-bit output almost full
+        DO => mem_high_out_1, -- Output data, width defined by DATA_WIDTH parameter
+        EMPTY => open, -- 1-bit output empty
+        FULL => open, -- 1-bit output full
+        RDCOUNT => rdcount_h_1, -- Output read count, width determined by FIFO depth
+        RDERR => open, -- 1-bit output read error
+        WRCOUNT => wrcount_h_1, -- Output write count, width determined by FIFO depth
+        WRERR => open, -- 1-bit output write error
+        DI => mem_high_in_1_reg, -- Input data, width defined by DATA_WIDTH parameter
+        RDCLK => clk_r, -- 1-bit input read clock
+        RDEN => ren_int_h, -- 1-bit input read enable
+        RST => rst, -- 1-bit input reset
+        WRCLK => clk_w, -- 1-bit input write clock
+        WREN => wen_bram -- 1-bit input write enable
+      );
 
 
 
