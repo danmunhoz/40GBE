e6f721cc45da763ef998abc44ee955c7869ad507
tanauan <tanauan.cardoso@gmail.com>
2018-01-24 17:59:33 -0200

    removendo arquivo desnecessário pkt_buffer.h

--- Hardware/sim/pkt_buffer.h
+++ Hardware/sim/pkt_buffer.h
@@ -58,8 +58,6 @@ SC_MODULE(pkt_buffer) {
 
   ofstream dbg_bip;
 
-  ofstream lane_dump_conf;
-
   sc_lv<64> block_in_old;
   sc_lv<2>  header_in_old;
 
@@ -165,28 +163,24 @@ SC_MODULE(pkt_buffer) {
 
           case 0:
             lane0 << buffer.str() << endl;
-            lane_dump_conf << buffer.str() << endl;
             // cout << buffer.str() << endl;
             block_counter++;
             bip_calculator (&lane0_bip, block_in, header_in);
             break;
           case 1:
             lane1 << buffer.str() << endl;
-            lane_dump_conf << buffer.str() << endl;
             // cout << buffer.str() << endl;
             block_counter++;
             bip_calculator (&lane1_bip, block_in, header_in);
             break;
           case 2:
             lane2 << buffer.str() << endl;
-            lane_dump_conf << buffer.str() << endl;
             // cout << buffer.str() << endl;
             block_counter++;
             bip_calculator (&lane2_bip, block_in, header_in);
             break;
           case 3:
             lane3 << buffer.str() << endl;
-            lane_dump_conf << buffer.str() << endl;
             // cout << buffer.str() << endl;
             block_counter++;
             bip_calculator (&lane3_bip, block_in, header_in);
@@ -222,7 +216,6 @@ SC_MODULE(pkt_buffer) {
           */
 
           lane0 << "10-" << SYNC_LANE0_LOW << lane0_bip << SYNC_LANE0_HIGH << ~lane0_bip << endl;
-          lane_dump_conf << "10-" << SYNC_LANE0_LOW << lane0_bip << SYNC_LANE0_HIGH << ~lane0_bip << endl;
           str_temp << SYNC_LANE0_LOW << lane0_bip << SYNC_LANE0_HIGH << ~lane0_bip;
           blk_temp = str_temp.str().c_str();
           hdr_temp = "10";
@@ -233,7 +226,6 @@ SC_MODULE(pkt_buffer) {
           // cout << "LANE0_BIP_ret: " << lane0_bip << endl << endl;
 
           lane1 << "10-" << SYNC_LANE1_LOW << lane1_bip << SYNC_LANE1_HIGH << ~lane1_bip << endl;
-          lane_dump_conf << "10-" << SYNC_LANE1_LOW << lane1_bip << SYNC_LANE1_HIGH << ~lane1_bip << endl;
           str_temp << SYNC_LANE1_LOW << lane1_bip << SYNC_LANE1_HIGH << ~lane0_bip;
           blk_temp = str_temp.str().c_str();
           hdr_temp = "10";
@@ -241,7 +233,6 @@ SC_MODULE(pkt_buffer) {
           bip_calculator (&lane1_bip, blk_temp, hdr_temp);
 
           lane2 << "10-" << SYNC_LANE2_LOW << lane2_bip << SYNC_LANE2_HIGH << ~lane2_bip << endl;
-          lane_dump_conf << "10-" << SYNC_LANE2_LOW << lane2_bip << SYNC_LANE2_HIGH << ~lane2_bip << endl;
           str_temp << SYNC_LANE2_LOW << lane2_bip << SYNC_LANE2_HIGH << ~lane0_bip;
           blk_temp = str_temp.str().c_str();
           hdr_temp = "10";
@@ -249,7 +240,6 @@ SC_MODULE(pkt_buffer) {
           bip_calculator (&lane2_bip, block_in_lv, hdr_temp);
 
           lane3 << "10-" << SYNC_LANE3_LOW << lane3_bip << SYNC_LANE3_HIGH << ~lane3_bip << endl;
-          lane_dump_conf << "10-" << SYNC_LANE3_LOW << lane3_bip << SYNC_LANE3_HIGH << ~lane3_bip << endl;
           str_temp << SYNC_LANE3_LOW << lane3_bip << SYNC_LANE3_HIGH << ~lane0_bip;
           blk_temp = str_temp.str().c_str();
           hdr_temp = "10";
@@ -300,13 +290,6 @@ SC_MODULE(pkt_buffer) {
       cout << "[pkt_buffer] ERROR opening lane3.txt" << endl;
     }
 
-    lane_dump_conf.open("lane_dump_conf.txt");
-    if (lane_dump_conf.is_open()){
-      cout << "[pkt_buffer] File lane_dump_conf.txt opened." << endl;
-    } else {
-      cout << "[pkt_buffer] ERROR opening lane_dump_conf.txt" << endl;
-    }
-
     SC_METHOD(rx);
     sensitive<<clock_in161.pos();
     dont_initialize();
@@ -323,7 +306,6 @@ SC_MODULE(pkt_buffer) {
     lane1.close();
     lane2.close();
     lane3.close();
-    lane_dump_conf.close();
   }
 
 };
