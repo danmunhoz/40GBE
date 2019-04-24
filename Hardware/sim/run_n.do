e4c309ca4277fb5ad39891400f24c5bc0fe6687d
tanauan <tanauan.cardoso@gmail.com>
2019-04-24 15:42:40 -0300

    automatização para simulações NASKIERAA PAAAE!

--- Hardware/sim/run_n.do
+++ Hardware/sim/run_n.do
@@ -1,11 +1,20 @@
-for {set i 11} {$i < 13} {incr i} {
-  for {set j 5} {$j < 6} {incr j} {
+
+set i_begin $::env(i_begin)
+set i_end $::env(i_end)
+set j_begin $::env(j_begin)
+set j_end $::env(j_end)
+set time_sim $::env(time_sim)
+set time_unit $::env(time_unit)
+
+
+for {set i $i_begin} {$i <= $i_end} {incr i} {
+  for {set j $j_begin} {$j <= $j_end} {incr j} {
 
     echo "*********************************************************"
     echo "*Executing with PL_CYCLE_MAX = $i and IPG_CYCLE_MAX = $j*"
     echo "*********************************************************"
 
-    do run.do $i $j 30 us
+    do run.do $i $j $time_sim $time_unit
     exec cp transcript transcript_pl${i}_ipg${j}
 
   }
--- Hardware/sim/run_simulation.sh
+++ Hardware/sim/run_simulation.sh
@@ -1,9 +1,34 @@
 # !/bin/bash
 
-rm -rf transcript* 
+if [ "$1" = "-h" ]
+then
+  echo "To use: ./run_simulation <min payload cycles*> <max payload cycles> <min IPG**> <max IPG> <simulation time> <simulation time unit>"
+  echo "*Min payload cycles must be at least 3"
+  echo "**Min IPG must be at least 1"
+elif [ $# -lt 6 ]
+then
+  echo "Parameters missing"
+  echo "To use: ./run_simulation <min payload cycles*> <max payload cycles> <min IPG**> <max IPG> <simulation time> <simulation time unit>"
+  echo "*Min payload cycles must be at least 3"
+  echo "**Min IPG must be at least 1"
+else
+  rm -rf transcript*
 
-module load modelsim
+  module load modelsim
 
-vsim -c -do run_n.do
+  i_begin=$1
+  i_end=$2
+  j_begin=$3
+  j_end=$4
+  time_sim=$5
+  time_unit=$6
 
+  export i_begin
+  export i_end
+  export j_begin
+  export j_end
+  export time_sim
+  export time_unit
 
+  vsim -c -do run_n.do
+fi
